24b25b4e9aaea28f6cdb6f2d5ffae134dff053ef
SasirSai <sasirsai2000@gmail.com>
2025-01-27 21:04:13 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello
+{
+    public static void main(String a[]){
+        int num = 69;
+        float num3= 4.5f;
+        System.out.println(num);
+        System.out.println(num3);
+    }
+}
